745eeca06e3e34e4b881e322513119f955cb6948
雅望 <hsingyu.sheep@gmail.com>
2018-10-02 21:34:51 +0800

    origin

---
+++ cloudfunctions/doTest/index.js
@@ -0,0 +1,15 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+const request=require('request')
+cloud.init()
+
+const db=cloud.database()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+    return await db.collection('User').add({
+		    data:event.testinfo
+		})
+}
+
+console.log(1)
