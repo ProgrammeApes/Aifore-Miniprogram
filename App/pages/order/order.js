7dc1639b68254d77aaab8ba3ecfd3581a6b920ca
leown <leown_wang@163.com>
2018-11-12 18:29:44 +0800

    add image arrry in order.js

--- App/pages/order/order.js
+++ App/pages/order/order.js
@@ -11,6 +11,7 @@ Page({
                name:"新品推荐",
                icon:'../../static/img/order/new-icon.png',
                prod: ['红豆红茶','绿茶','红豆红茶','绿茶'],
+               cupImage:['../../static/img/order/cup.png','../../static/img/order/cup.png','../../static/img/order/cup.png','../../static/img/order/cup.png'],
                s$:[12,10,12,10],
                b$:[14,12,14,12],
                sNum:[0,0,0,0],
@@ -20,6 +21,7 @@ Page({
                name:'健康果茶',
                icon:'../../static/img/order/jkgc.png',
                prod: ['黑糖鲜奶','aifore'],
+               cupImage:['../../static/img/order/cup.png','../../static/img/order/cup.png','../../static/img/order/cup.png','../../static/img/order/cup.png'],
                s$:[12,10],
                b$:[15,13],
                sNum:[0,0],
@@ -80,8 +82,8 @@ Page({
             aCost:this.data.aCost,
 
         })
-    },
-    smallMin: function (e) {
+  },
+  smallMin: function (e) {
         if(this.data.array[e.currentTarget.dataset.id].sNum[e.currentTarget.dataset.idx] > 0){
             this.data.array[e.currentTarget.dataset.id].sNum[e.currentTarget.dataset.idx]--;
             this.data.aCost -=this.data.array[e.currentTarget.dataset.id].s$[e.currentTarget.dataset.idx];
@@ -92,8 +94,8 @@ Page({
             aCount:this.data.aCount,
             aCost:this.data.aCost,
         })
-    },
-    smallAdd: function (e) {
+  },
+  smallAdd: function (e) {
         this.data.array[e.currentTarget.dataset.id].sNum[e.currentTarget.dataset.idx]++;
         this.data.aCost +=this.data.array[e.currentTarget.dataset.id].s$[e.currentTarget.dataset.idx];
         this.data.aCount++;
